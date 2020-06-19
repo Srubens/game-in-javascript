a71f582617a2c55dbe0939f999f5437323d3b3aa
Rubens Filipe <rubensfilipe6@gmail.com>
2020-06-19 11:09:05 -0300

    add a condicao de pontuacao

--- config.js
+++ config.js
@@ -14,6 +14,13 @@
 	let velocidadeBolaPosicaoX = 3
 	let velocidadeBolaPosicaoY = 3
 
+	let posicaoJogador1 = 40;
+	let posicaoJogador2 = 40;
+
+	// PONTUACAO
+	let pontuacaoJogador1 = 0; 
+	let pontuacaoJogador2 = 0; 
+
 	function executar(){
 
 		let $folha = document.querySelector('[data-js="folha-js"]');
@@ -26,6 +33,11 @@
 		let diametroBola = 10;
 
 		let contador = 0;
+
+		let espessuraRaquete = 11;
+		let alturaRaquete = 100;
+
+		let efeitoRaquete = 0.3;
 
 		//CAMPO
 		$areaDesenho.fillStyle = "#006600";
@@ -44,6 +56,11 @@
 
 		// console.log($folha,$areaDesenho);
 
+		// RAQUETE 01
+		$areaDesenho.fillStyle = "#fff";
+		$areaDesenho.fillRect(0, posicaoJogador1, espessuraRaquete, alturaRaquete);
+		$areaDesenho.fillRect(larguraCampo - espessuraRaquete, posicaoJogador2, espessuraRaquete, alturaRaquete);
+
 		// VERIFICA A LATERAL SUPERIOR
 		if( posicaoBolaY < 0 && velocidadeBolaPosicaoY < 0 ){
 			velocidadeBolaPosicaoY = -velocidadeBolaPosicaoY
@@ -53,6 +70,35 @@
 		if( posicaoBolaY > alturaCampo && velocidadeBolaPosicaoY > 0 ){
 			velocidadeBolaPosicaoY = -velocidadeBolaPosicaoY
 		}
+
+		// VERIFICA SE O JOGADOR 2 FEZ PONTO
+		if( posicaoBolaX < 0 ){
+			if( posicaoBolaY > posicaoJogador1 && posicaoBolaY < posicaoJogador1 + alturaRaquete ){
+				//REBATER A BOLA
+				velocidadeBolaPosicaoX = -velocidadeBolaPosicaoX;
+				
+				let diferencaY = posicaoBolaY - ( posicaoJogador1 + alturaRaquete / 2 );
+				
+				velocidadeBolaPosicaoY = diferencaY * efeitoRaquete
+			}else{
+				// PONTO JOGADOR 2
+				pontuacaoJogador2 = pontuacaoJogador2 + 1;
+				//colocar bola no centro 
+			}
+		}
+
+		// VERIFICA SE O JOGADOR 1 FEZ PONTO
+		if( posicaoBolaX > larguraCampo  ){
+			if( posicaoBolaY > posicaoJogador2 && posicaoBolaY < posicaoJogador2 + alturaRaquete ){
+				velocidadeBolaPosicaoX = -velocidadeBolaPosicaoX;
+				let diferencaY = posicaoBolaY - ( posicaoJogador2 + alturaRaquete / 2 );
+				velocidadeBolaPosicaoY = diferencaY * efeitoRaquete
+			}else{
+				pontuacaoJogador1 = pontuacaoJogador1+ 1;
+				//colocar bola no centro 
+			}
+		}
+
 
 	}
 
