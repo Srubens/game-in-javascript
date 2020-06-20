3e5ffcfe55a2c71b6fea7b536d68f7ba84b48a10
Rubens Filipe <rubensfilipe6@gmail.com>
2020-06-20 18:39:17 -0300

    ajustando o config.js, retirando style, e modificando o index

--- config.js
+++ config.js
@@ -3,8 +3,9 @@
 	"use script";
 
 	window.onload = function(){
-		setInterval(executar, 1000 / 30);
+		setInterval(principal, 1000 / 30);
 	}
+
 
 	let $folha = document.querySelector('[data-js="folha-js"]');
 	let $areaDesenho = $folha.getContext("2d");
@@ -39,12 +40,70 @@
 
 	let velocidadeJogador2 = 5;
 
-	$folha.addEventListener("mousemove", function(e){
+
+	$folha.addEventListener("mousemove", (e) => {
 		posicaoJogador1 = e.clientY - alturaRaquete / 2;
 	});
 
 
-	function executar(){
+	function calcular(){
+
+		desenhar();
+
+		// VERIFICA A LATERAL SUPERIOR
+		if( posicaoBolaY < 0 && velocidadeBolaPosicaoY < 0 ){
+			velocidadeBolaPosicaoY = -velocidadeBolaPosicaoY
+		}
+
+		// VERIFICA A LATERAL INFERIOR
+		if( posicaoBolaY > alturaCampo && velocidadeBolaPosicaoY > 0 ){
+			velocidadeBolaPosicaoY = -velocidadeBolaPosicaoY
+		}
+
+		// // VERIFICA SE O JOGADOR 2 FEZ PONTO
+		if( posicaoBolaX < 0 ){
+			if( posicaoBolaY > posicaoJogador1 && posicaoBolaY < posicaoJogador1 + alturaRaquete ){
+				//REBATER A BOLA
+				velocidadeBolaPosicaoX = -velocidadeBolaPosicaoX;
+				
+				let diferencaY = posicaoBolaY - ( posicaoJogador1 + alturaRaquete / 2 );
+				
+				velocidadeBolaPosicaoY = diferencaY * efeitoRaquete
+			}else{
+				// PONTO JOGADOR 2
+				pontuacaoJogador2 = pontuacaoJogador2 + 1;
+				//colocar bola no centro 
+				continuar();
+			}
+		}
+
+		// VERIFICA SE O JOGADOR 1 FEZ PONTO
+		if( posicaoBolaX > larguraCampo  ){
+			if( posicaoBolaY > posicaoJogador2 && posicaoBolaY < posicaoJogador2 + alturaRaquete ){
+				velocidadeBolaPosicaoX = -velocidadeBolaPosicaoX;
+				let diferencaY = posicaoBolaY - ( posicaoJogador2 + alturaRaquete / 2 );
+				velocidadeBolaPosicaoY = diferencaY * efeitoRaquete
+			}else{
+				pontuacaoJogador1 = pontuacaoJogador1+ 1;
+				//colocar bola no centro 
+				continuar();
+			}
+		}
+
+		//ATUALIZA A POSIÇÃO DO JOGADOR 2
+		if( posicaoJogador2 + alturaRaquete / 2 < posicaoBolaY ){
+			posicaoJogador2 = posicaoJogador2 + velocidadeJogador2;
+		}else{
+			posicaoJogador2 = posicaoJogador2 - velocidadeJogador2;
+		}
+
+	}
+
+	function principal(){
+		calcular();
+	}
+
+	function desenhar(){
 
 		//CAMPO
 		$areaDesenho.fillStyle = "#006600";
@@ -72,59 +131,28 @@
 		$areaDesenho.fillText("Jogador 1 - " + pontuacaoJogador1 + " pontos", 100, 100);
 		$areaDesenho.fillText("Jogador 2 - " + pontuacaoJogador2 + " pontos", larguraCampo - 200, 100);
 
-		// VERIFICA A LATERAL SUPERIOR
-		if( posicaoBolaY < 0 && velocidadeBolaPosicaoY < 0 ){
-			velocidadeBolaPosicaoY = -velocidadeBolaPosicaoY
-		}
-
-		// VERIFICA A LATERAL INFERIOR
-		if( posicaoBolaY > alturaCampo && velocidadeBolaPosicaoY > 0 ){
-			velocidadeBolaPosicaoY = -velocidadeBolaPosicaoY
-		}
-
-		// VERIFICA SE O JOGADOR 2 FEZ PONTO
-		if( posicaoBolaX < 0 ){
-			if( posicaoBolaY > posicaoJogador1 && posicaoBolaY < posicaoJogador1 + alturaRaquete ){
-				//REBATER A BOLA
-				velocidadeBolaPosicaoX = -velocidadeBolaPosicaoX;
-				
-				let diferencaY = posicaoBolaY - ( posicaoJogador1 + alturaRaquete / 2 );
-				
-				velocidadeBolaPosicaoY = diferencaY * efeitoRaquete
-			}else{
-				// PONTO JOGADOR 2
-				pontuacaoJogador2 = pontuacaoJogador2 + 1;
-				//colocar bola no centro 
-				posicaoBolaX = larguraCampo / 2;
-				posicaoBolaY = larguraCampo / 2;
-				velocidadeBolaPosicaoX = -velocidadeBolaPosicaoX;
-				velocidadeBolaPosicaoY = 3;
-			}
-		}
-
-		// VERIFICA SE O JOGADOR 1 FEZ PONTO
-		if( posicaoBolaX > larguraCampo  ){
-			if( posicaoBolaY > posicaoJogador2 && posicaoBolaY < posicaoJogador2 + alturaRaquete ){
-				velocidadeBolaPosicaoX = -velocidadeBolaPosicaoX;
-				let diferencaY = posicaoBolaY - ( posicaoJogador2 + alturaRaquete / 2 );
-				velocidadeBolaPosicaoY = diferencaY * efeitoRaquete
-			}else{
-				pontuacaoJogador1 = pontuacaoJogador1+ 1;
-				//colocar bola no centro 
-				posicaoBolaX = larguraCampo / 2;
-				posicaoBolaY = larguraCampo / 2;
-				velocidadeBolaPosicaoX = -velocidadeBolaPosicaoX;
-				velocidadeBolaPosicaoY = 3;
-			}
-		}
-
-		//ATUALIZA A POSIÇÃO DO JOGADOR 2
-		if( posicaoJogador2 + alturaRaquete / 2 < posicaoBolaY ){
-			posicaoJogador2 = posicaoJogador2 + velocidadeJogador2;
-		}else{
-			posicaoJogador2 = posicaoJogador2 - velocidadeJogador2;
-		}
-
 	}
+
+	function continuar(){
+		posicaoBolaX = larguraCampo / 2;
+		posicaoBolaY = larguraCampo / 2;
+		velocidadeBolaPosicaoX = -velocidadeBolaPosicaoX;
+		velocidadeBolaPosicaoY = 3;
+	}
+
+	function verificaPontuacao(posicaoBolaX, valor, posicaoJogador, pontuacaoJogador){
+		if( posicaoBolaX > valor  ){
+			if( posicaoBolaY > posicaoJogador && posicaoBolaY < posicaoJogador + alturaRaquete ){
+				velocidadeBolaPosicaoX = -velocidadeBolaPosicaoX;
+				let diferencaY = posicaoBolaY - ( posicaoJogador + alturaRaquete / 2 );
+				velocidadeBolaPosicaoY = diferencaY * efeitoRaquete
+			}else{
+				pontuacaoJogador = pontuacaoJogador+ 1;
+				//colocar bola no centro 
+				continuar();
+			}
+		}
+	}
+
 
 })(window, document)
--- readme.md
+++ readme.md
@@ -5,6 +5,6 @@ com intuito de pegar pratica nos estudos.
 
 <em>Regas do Jogo</em>
 
-1 - Ao bater nas laterais do campo a bola deverá ser rebatida no sentido oposto.
-2 - Ao bater no jogador a bola deverá ser rebatida no sentido oposto.
-3 - Ao cruzar a linha do adversário (sem bater no jogador) deverá somar 1 ponto e iniciar a bola no meio campo.
+1 - Ao bater nas laterais do campo a bola deverá ser rebatida no sentido oposto.<br>
+2 - Ao bater no jogador a bola deverá ser rebatida no sentido oposto.<br>
+3 - Ao cruzar a linha do adversário (sem bater no jogador) deverá somar 1 ponto e iniciar a bola no meio campo.<br>
